7e15bf78b53eddee1f5a2fd5c532ad89661895d0
sazzad110 <sazzadhasan610@gmail.com>
2024-09-14 10:20:07 +0600

    templated added

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from polls import views
-from django.conf.urls import include
+from first_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("polls/",include("polls.urls"))
+    path("index/",views.index,name="index")
+
 
 ]
