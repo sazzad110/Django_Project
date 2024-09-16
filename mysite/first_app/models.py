2c0a74c70d8edfee7211dca1500f5c59bde34393
sazzad110 <sazzadhasan610@gmail.com>
2024-09-16 19:52:43 +0600

    operation on mode

--- mysite/first_app/models.py
+++ mysite/first_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Muscian(models.Model):
     # id = models.AutoField(Primary_key=True) # hidden auto create hobe
-    first_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30,)
     last_name = models.CharField(max_length=38)
     instrument = models.CharField(max_length=48)
 
@@ -14,4 +14,14 @@ class Album(models.Model):
     artist = models.ForeignKey(Muscian,on_delete=models.CASCADE)        # foreign key
     name = models.CharField(max_length=49)
     release_date = models.DateField()
-    nums_stars = models.IntegerField()
+
+    # rating will be our choice
+    ratings = (
+        (1,"good"),
+        (2,"bad"),
+        (3,"very good")
+    )
+    nums_stars = models.IntegerField(choices=ratings)
+
+    def __str__(self) -> str:
+        return self.name + ", Rating: " + str(self.nums_stars)
