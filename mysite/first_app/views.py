aa97466a2ee7ce613c456f6510d9fe0c45db25af
sazzad110 <sazzadhasan610@gmail.com>
2024-09-17 09:39:54 +0600

    Play with database

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Muscian,Album            # i want to show all musician from my database
 
 def index(request):
-    diction = {"text_1": "Sending value from view"}     # to show this in index.html
+    musician_list = Muscian.objects.order_by('first_name')
+    diction = {
+        "text_1":"List of Musician",
+        "musician":musician_list
+    }
     return render(request,'first_app/index.html',context=diction)
