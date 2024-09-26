816205dfd1f009d9911c94a777bb552e07c4a815
sazzad110 <sazzadhasan610@gmail.com>
2024-09-26 10:11:11 +0600

    Added form submit button and functionality

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -8,6 +8,21 @@ def index(request):
     return HttpResponse("Hello World")
 
 def form(request):
-    form_obj = UserForm()   # create obj of my form
+    form_obj = UserForm()   # create obj of my form to store an empty form
     dict = {'test_form':form_obj}       # to pass this form in my html page
+
+    # if anyone click submit then POST mehtod check
+    if request.method == 'POST':
+        form_obj = UserForm(request.POST)   # to get submitted information
+
+        if form_obj.is_valid():
+            user_name = form_obj.cleaned_data['name']
+            user_email = form_obj.cleaned_data['email']
+            user_dob = form_obj.cleaned_data['user_dob']
+
+            dict.update({'user_name':user_name})
+            dict.update({'user_email':user_email})
+            dict.update({'user_dob':user_dob})
+            dict.update({'is_submitted':'yes'})
+
     return render(request,'first_app/index.html',context=dict)
